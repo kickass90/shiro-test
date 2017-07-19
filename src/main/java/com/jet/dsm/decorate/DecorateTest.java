e95cbcb2b7af15d798751e21995f198599921b30
zkf <1175354924@qq.com>
2017-07-19 13:27:36 +0800

    shiro

--- src/main/java/com/jet/dsm/decorate/DecorateTest.java
+++ src/main/java/com/jet/dsm/decorate/DecorateTest.java
@@ -12,8 +12,8 @@ public class DecorateTest {
     public static void main(String[] args) {
         Human human = new Man();
         Decorate decorate = new BlackShoes(human);
-        decorate.gotoRoom();
         decorate = new DrinkMilk(decorate);
+        decorate.gotoRoom();
         decorate.gotoKitchen();
     }
 }
